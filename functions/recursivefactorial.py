e8bf70dd9e394b4782b2bc6215375820ab57bad9
goyalanshika2507-dot <goyalanshika.2507@gmail.com>
2026-08-03 22:06:17 +0530

    pattern programs

--- functions/recursivefactorial.py
+++ functions/recursivefactorial.py
@@ -5,12 +5,4 @@ def factorial(n):
         return n*factorial(n-1)
 print(factorial(3))
 print(factorial(5))
-   
-
-
-
-
-
-
-
-   
+  
---
+++ patterns/starpattern5.py
@@ -0,0 +1,7 @@
+n=int(input("enter no"))
+for i in range(n):
+    print(" "*(n-i-1),end=" ")
+    print("*"*(2*i+1))
+for i in range(n-2,-1,-1):
+    print(" "*(n-i-1),end=" ")
+    print("*"*(2*i+1))
